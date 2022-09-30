b88e351ddc22c48d46554bb9170db2238e1b7b8c
Daniel_Sanabria06 <sanabria3210@gmail.com>
2022-09-30 14:01:04 -0500

    first commit

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const router = express.Router();
+//TODO: quitar los //
+import { 
+    registrar,
+    autenticar,
+  //  confirmar,
+    // olvidePassword, 
+    comprobarToken,
+  //  nuevoPassword,
+    perfil
+} from '../controllers/usuarioController.js'
+
+import CheckAuth from "../Middleware/CheckAtuh.js"
+
+// Autenticación, Registro y Confirmación de usuarios
+//TODO:quitar los //
+router.post("/", registrar); //Crea un nuevo usuario 
+router.post("/login", autenticar); 
+//router.get("/confirmar/:token", confirmar); 
+//router.post("/olvide-password", olvidePassword);
+//router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword)
+router.get("/perfil", CheckAuth, perfil)
+
+export default router;
