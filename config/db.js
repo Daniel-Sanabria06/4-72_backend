0efeec8453f9c104c88bb9ca942fb6f475051f2e
Daniel_Sanabria06 <sanabria3210@gmail.com>
2023-09-11 15:28:03 -0500

    cambiando mongo

--- config/db.js
+++ config/db.js
@@ -2,12 +2,19 @@ import mongoose from "mongoose";
 
 const contectarDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI , {
+        mongoose.connect(process.env.MONGO_URI , {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             })
+            .then(() => {
+                console.log('Conexión exitosa a MongoDB');
+              })
+             /* .catch((error) => {
+                console.error('Error en la conexión a MongoDB:', error);
+              });
         const url = `${connection.connection.host}:${connection.connection.port}`
         console.log(`MongoDB conectado en ${url}`)
+        */
     } catch (error) {
         console.log(`error: ${error.message}`);
         process.exit(1);
